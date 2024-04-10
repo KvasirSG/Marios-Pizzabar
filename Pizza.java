951a19fcd88163cce38827bb1f9a5166851192b9
Nikolaj Pirum <nikolajpirum.10@gmail.com>
2024-04-10 11:21:41 +0200

    added comment to pizza

--- Pizza.java
+++ Pizza.java
@@ -15,44 +15,44 @@ public class Pizza implements Serializable
       this.ingredient = ingredient; 
       this.price = price;
    }
-  
-   public int getPizzaID()
+   // method for printing pizza number
+   public int getPizzaID() 
    {
       return pizzaID;
    }
-   
-   public String getName()
+   // method for printing pizza name
+   public String getName() 
    {
       return name;
    }
-   
-   public void setName(String name)
+   //method for setting pizza name 
+   public void setName(String name) 
    {
       this.name = name;
    }
-   
-   public String getIngredient()
+   // method for printing pizza ingrediens 
+   public String getIngredient() 
    {
       return ingredient; 
    }
-   
-   public void setIngredient(String ingredient)
+   // method setting pizza ingrediens
+   public void setIngredient(String ingredient) 
    {
       this.ingredient = ingredient;
    }
-   
-   public double getPrice()
+   // method for printing pizza price
+   public double getPrice() 
    {
       return price;
    }
-   
-   public void setPrice(double price)
+   // method for setting pizza price
+   public void setPrice(double price) 
    {
       this.price = price;
    }
 
-   @Override
-   public String toString() {
+   public String toString() 
+   {
       // Mimics an array format: [pizzaID, name, ingredient, price]
       return String.format("[%d, %s, %s, %.2f]", pizzaID, name, ingredient, price);
    }
