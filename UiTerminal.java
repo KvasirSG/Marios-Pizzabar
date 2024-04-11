f336a0bb1ae543b0537cb3fe3a652d29e25c2dc3
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-04-11 10:57:49 +0100

    added some stuff to the terminal

--- UiTerminal.java
+++ UiTerminal.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Console UI for the pizza ordering system
@@ -7,13 +9,49 @@ public class UiTerminal {
     UiController uiController;
     public UiTerminal(UiController uiController){
         this.uiController = uiController;
+        this.uiController.addPizzaToMenu("Margherita","Tomatsauce, mozzarella, basilikum", 8.99);
+        this.uiController.addPizzaToMenu("Pepperoni","Tomatsauce, mozzarella, pepperoni", 9.99);
+        this.uiController.addPizzaToMenu("Vegetarian","Tomat og Ost", 9.49);
+        uInputHandler();
     }
-    public void displayMenu() {
+    private void displayMenu() {
         List<Pizza> pizzaList = uiController.getPizzaMenu();
         System.out.println(" ");
         System.out.println("Menu:");
         for (Pizza pizza : pizzaList) {
-            System.out.println(pizza);
+            System.out.println(pizza.getPizzaID()+". "+pizza.getName()+": "+pizza.getIngredient()+"..."+ pizza.getPrice()+",-");
         }
     }
+
+    // Method to display options in the main menu
+    private void displayOptions() {
+        List<String> options = new ArrayList<>();
+        System.out.println("\nOptions:");
+        options.add("Display Menu");
+        int index = 1;
+        for (String option:options){
+            System.out.println(index+". "+option);
+            index++;
+        }
+        System.out.print("Enter your choice: ");
+    }
+
+    private void uInputHandler(){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            displayOptions();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+
+            switch (choice) {
+                case 1:
+                    displayMenu();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        }
+    }
+
+
 }
