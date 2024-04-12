bbf2bc6f6012fbcdb40c84c4267d357000a14b00
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-04-12 13:35:15 +0100

    made changes to the terminal to support order management

--- UiTerminal.java
+++ UiTerminal.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Console UI for the pizza ordering system
@@ -27,6 +25,8 @@ public class UiTerminal {
         options.add("Display Menu");
         options.add("Add Pizza to Menu");
         options.add("Remove Pizza from Menu");
+        options.add("View current orders");
+        options.add("Add Order");
         options.add("Exit");
         int index = 1;
         for (String option:options){
@@ -36,7 +36,7 @@ public class UiTerminal {
         System.out.print("Enter your choice: ");
     }
 
-    private void displayAddPizzToMenu(){
+    private void displayAddPizzaToMenu(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Add a pizza to menu by name, ingredients, price");
         System.out.print("Enter name of the Pizza: ");
@@ -48,6 +48,53 @@ public class UiTerminal {
         scanner.nextLine(); // Consume newline character
         uiController.addPizzaToMenu(pizzaName,pizzaIngredients,pizzaPrice);
         displayMenu();
+
+    }
+
+    private void displayRemovePizzaFromMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the id of the pizza you want to remove: ");
+        int pizzaID = scanner.nextInt();
+        scanner.nextLine(); // Consume newline character
+        uiController.removePizzaFromMenu(pizzaID);
+        displayMenu();
+    }
+
+    private void displayOrders(){
+        List<Order> orders = uiController.getAllOrders();
+        orders.sort((Comparator.comparingInt(Order::getPriority)));
+        for (Order order:orders){
+            System.out.println(order.getPriority()+". "+order.getOrderID()+" - "+order.getPizzas()+" - "+order.getOrderTime());
+        }
+    }
+
+    private void displaySelectedPizzaList(){
+        List<Pizza> pizzaList = uiController.getPizzaList();
+        System.out.println(" ");
+        System.out.println("Selected Pizzas:");
+        for (Pizza pizza : pizzaList) {
+            System.out.println(pizza.getPizzaID()+". "+pizza.getName()+": "+pizza.getIngredient()+"..."+ pizza.getPrice()+",-");
+        }
+    }
+
+    private void displayAddOrder(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Making new order:");
+        while (true){
+            System.out.print("Add Pizza to order (type 0 to finish): ");
+            int input = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+            if (input == 0){
+                break;
+            } else {
+                uiController.addPizzaToList(input);
+                displaySelectedPizzaList();
+            }
+        }
+        if (!uiController.getPizzaList().isEmpty()) {
+            uiController.addPizzaOrder();
+            displayOrders();
+        }
 
     }
 
@@ -63,9 +110,18 @@ public class UiTerminal {
                     displayMenu();
                     break;
                 case 2:
-                    displayAddPizzToMenu();
+                    displayAddPizzaToMenu();
                     break;
                 case 3:
+                    displayRemovePizzaFromMenu();
+                    break;
+                case 4:
+                    displayOrders();
+                    break;
+                case 5:
+                    displayAddOrder();
+                    break;
+                case 6:
                     return;
                 default:
                     System.out.println("Invalid choice. Please enter a valid option.");
