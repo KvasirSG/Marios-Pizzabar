64d244ad48b3af52430157fc7d007853f8d13a0d
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-04-12 09:26:56 +0100

    WIP added addpizza functionality

--- UiTerminal.java
+++ UiTerminal.java
@@ -25,12 +25,30 @@ public class UiTerminal {
         List<String> options = new ArrayList<>();
         System.out.println("\nOptions:");
         options.add("Display Menu");
+        options.add("Add Pizza to Menu");
+        options.add("Remove Pizza from Menu");
+        options.add("Exit");
         int index = 1;
         for (String option:options){
             System.out.println(index+". "+option);
             index++;
         }
         System.out.print("Enter your choice: ");
+    }
+
+    private void displayAddPizzToMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add a pizza to menu by name, ingredients, price");
+        System.out.print("Enter name of the Pizza: ");
+        String pizzaName = scanner.nextLine();
+        System.out.print("Enter the ingredients of the Pizza: ");
+        String pizzaIngredients = scanner.nextLine();
+        System.out.print("Enter the price: ");
+        double pizzaPrice = scanner.nextDouble();
+        scanner.nextLine(); // Consume newline character
+        uiController.addPizzaToMenu(pizzaName,pizzaIngredients,pizzaPrice);
+        displayMenu();
+
     }
 
     private void uInputHandler(){
@@ -44,6 +62,11 @@ public class UiTerminal {
                 case 1:
                     displayMenu();
                     break;
+                case 2:
+                    displayAddPizzToMenu();
+                    break;
+                case 3:
+                    return;
                 default:
                     System.out.println("Invalid choice. Please enter a valid option.");
             }
