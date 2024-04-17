9242a1dd660fa5bb2913f8fb86b818c4362753b4
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-04-17 15:47:56 +0100

    rewritten to support of a remove button

--- PizzaUI/RightPanel.java
+++ PizzaUI/RightPanel.java
@@ -8,14 +8,37 @@ import javax.swing.*;
 import java.awt.*;
 
 public class RightPanel extends JPanel {
-    private JList<Pizza> selectedPizzasList = new JList<>(); // You'll need a custom list model
-    private JButton addOrderButton = new JButton("Add to Order");
+    private JTable pizzaTable;
+    private PizzaTableModel pizzaTableModel;
+    private UiController uiController;
+    private JButton addOrderButton = new JButton("Tilføj Order");
+    private JTextField subTotalField;
 
     public RightPanel(UiController uiController) {
+        this.uiController = uiController;
         setLayout(new BorderLayout());
-        selectedPizzasList.setListData(uiController.getPizzaList().toArray(new Pizza[0]));
-        add(new JScrollPane(selectedPizzasList), BorderLayout.CENTER);
-        add(addOrderButton, BorderLayout.SOUTH);
+        pizzaTableModel = new PizzaTableModel(uiController.getPizzaList(),ButtonType.RPIZZA);
+        pizzaTable = new JTable(pizzaTableModel);
+
+        // Setting the custom renderer and editor for the "Remove" button column
+        buttonRender(pizzaTable);
+        add(new JScrollPane(pizzaTable), BorderLayout.CENTER);
+
+
+
+        subTotalField = new JTextField(String.valueOf(subTotal()));
+        subTotalField.setEditable(false);
+        // Create a panel to hold the field and the button
+        JPanel southPanel = new JPanel(new GridLayout(2, 1)); // 2 rows, 1 column
+        // add the addOrderButton and the textfield to the panel
+        southPanel.add(new JLabel("Subtotal:"));
+        southPanel.add(subTotalField);
+        addOrderButton.setOpaque(true);
+        southPanel.add(addOrderButton,BorderLayout.EAST);
+
+        // add the panel
+        add(southPanel, BorderLayout.SOUTH);
+
     }
 
     public JButton getAddOrderButton() {
@@ -23,7 +46,21 @@ public class RightPanel extends JPanel {
     }
 
     public void updateSelectedPizzaList(List<Pizza> pizzas){
-        selectedPizzasList.setListData(pizzas.toArray(new Pizza[0]));
+        pizzaTableModel = new PizzaTableModel(pizzas, ButtonType.RPIZZA); // Re-initialize pizzaTableModel with new pizzas
+        pizzaTable.setModel(pizzaTableModel);
+        subTotalField.setText(String.valueOf(subTotal()));
+        buttonRender(pizzaTable);
+    }
+    private double subTotal(){
+        double temp=0;
+        for (Pizza pizza: uiController.getPizzaList()){
+            temp+= pizza.getPrice();
+        }
+            return temp;
+    }
+    public void buttonRender(JTable pizzaTable){
+        pizzaTable.getColumn("Fjern").setCellRenderer(new ButtonRenderer());
+        pizzaTable.getColumn("Fjern").setCellEditor(new ButtonEditor(new JCheckBox(), uiController, this,ButtonType.RPIZZA));
     }
 }
 
